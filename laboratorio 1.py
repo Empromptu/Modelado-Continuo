e9bc3ae3a816f4e9ebf074270571391d6d5a8dde
Juan I <141973423+Empromptu@users.noreply.github.com>
2025-03-26 19:32:22 -0300

    Add files via upload
    Ejercicios 16 y 9

--- laboratorio 1.py
+++ laboratorio 1.py
@@ -99,7 +99,125 @@ plt.show()
 
 #%%
 
-# Ejercicio 3
+# Ejercicio 9
+
+def Hutchinson_euler(r, k, h, t_max, tau, N0 = 0.5):
+    t_values = np.arange(0, t_max + h, h)
+    N_values = np.zeros_like(t_values)
+    N_values[0] = N0
+
+    for i in range(1,len(t_values)):
+        if (i- tau) < 0 :
+            N_values[i] = N0
+        else :
+            N_values[i] = N_values[i-1] + h * r * N_values[i-1] * (1-(N_values[i-1-tau]/k))
+        
+    return t_values , N_values
+
+# Parámetros
+r = 0.5
+tau_values = [1, 5, 10]  
+k = 100
+t_max = 50
+h = 0.1
+
+# Funcíon de Hutchinson sin delay
+def Hutchinson_euler_sin_delay(r, k, h, t_max, N0 = 0.5):
+    t_values = np.arange(0, t_max + h, h)
+    N_values = np.zeros_like(t_values)
+    N_values[0] = N0
+
+    for i in range(1,len(t_values)):
+        N_values[i] = N_values[i-1] + h * r * N_values[i-1] * (1-(N_values[i-1-tau]/k))
+        
+    return t_values , N_values
+
+
+#Graficar soluciones para distintos tau y h
+plt.figure(figsize=(12, 8))
+t_sin_delay , y_sin_delay = Hutchinson_euler_sin_delay(r, k, h, t_max)
+plt.plot(t_sin_delay, y_sin_delay, marker='o', linestyle='--')
+for tau in tau_values:
+    t_euler , y_euler = Hutchinson_euler(r, k, h, t_max, tau)
+    plt.plot(t_euler, y_euler, marker='o', linestyle='--', label=f'tau={tau}')
+
+    plt.title("Solucion de la ecuacion de Hutchinson con método de Euler")
+    plt.xlabel("t")
+    plt.ylabel("N(t)")
+    plt.legend()
+    plt.grid()
+
+plt.tight_layout()
+plt.show()
+
+#%%
+
+# Ejercicio 16
+
+def bouncing_ball(v0,y0,t_max,h):
+    t_values = np.arange(0,t_max + h, h )
+    y_values = np.zeros_like(t_values)
+    v_values = np.zeros_like(t_values)
+    
+    y_values[0] = y0
+    v_values[0] = v0
+    
+    for i in range(1,len(t_values)):
+        v_values[i] = v_values[i-1] - 10*h
+        y_values[i] = y_values[i-1] + h* v_values[i-1]
+        
+        if y_values[i] < 0:
+            y_values[i] = 0
+            v_values[i] = -0.8*v_values[i]
+            
+    return t_values, y_values, v_values 
+    
+# Parámetros
+v0=0
+y0=10
+t_max = 50
+h = 0.001
+
+# Graficar
+
+t_euler, y_euler, v_euler = bouncing_ball(v0, y0, t_max, h)
+
+plt.figure(figsize=(12, 8))
+
+# Graficar la posición (y)
+plt.plot(t_euler, y_euler, label='Posición (y)', color='b', marker='o', linestyle='--')
+
+
+plt.title("Solución de Bouncing Ball con Método de Euler")
+plt.xlabel("Tiempo (t)")
+plt.ylabel("Posición (y) ")
+plt.legend()
+plt.grid()
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
